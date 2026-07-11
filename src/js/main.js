01b6907bd486194562995bdd5422ce33e724dc41
James Frazier <hello@webdevjames.com>
2026-07-10 18:25:41 -0700

    feat: Hero changes and Tooltips.
    Activated header BG sooner. Added social icons to hero, adjusted font sizes. Added tooltips for the cards in the orange section.

--- src/js/main.js
+++ src/js/main.js
@@ -5,32 +5,31 @@ import "../scss/styles.scss";
 // Import necessary BS elements (not all)
 import { Collapse } from "bootstrap";
 import { Offcanvas } from "bootstrap";
+import { Tooltip } from "bootstrap";
+
+// Initialize BS tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new Tooltip(tooltipTriggerEl));
 
 // Intersection observer for Header only
-const hero = document.querySelector(".hero");
+const sentinel = document.querySelector("#page-top-sentinel");
 const header = document.querySelector("#header");
-const flyoutMenu = document.querySelector(".mobile-drawer-glass");
 
-const observerOptions = {
-	root: null,
-	rootMargin: "-125px 0px 0px 0px",
-	threshold: 0,
-};
+const headerObserver = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			// If the sentinel is NOT visible, it means the user has scrolled down
+			if (!entry.isIntersecting) {
+				header.classList.add("scrolled");
+			} else {
+				header.classList.remove("scrolled");
+			}
+		});
+	},
+	{ rootMargin: "20px 0px 0px 0px" },
+); // Fires exactly 20px into the scroll!
 
-const observer = new IntersectionObserver((entries) => {
-	entries.forEach((entry) => {
-		// If the hero div is no longer intersecting (scrolled past)
-		if (!entry.isIntersecting) {
-			header.classList.add("scrolled");
-			flyoutMenu.classList.add("scrolled");
-		} else {
-			header.classList.remove("scrolled");
-			flyoutMenu.classList.remove("scrolled");
-		}
-	});
-}, observerOptions);
-
-observer.observe(hero);
+headerObserver.observe(sentinel);
 
 // Plugins
 import "./countdown.js";
